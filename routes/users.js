a9cc40a39001f636c171aae58302533ba361754d
Jai Agarwal <jai.agarwal2022@gmail.com>
2022-07-20 12:49:36 +0530

    Passport JWT

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 const bodyParser = require('body-parser');
 const User = require('../models/user');
 var passport = require('passport');
+var authenticate = require('../authenticate')
 
 
 var router = express.Router();
@@ -20,7 +21,7 @@ router.post('/signup', (req, res, next) => {
         res.json({err: err})
       }
       else {
-        passport.authenticate('local')(req,res, () => {
+        passport.authenticate('local', {session: false})(req,res, () => {
           res.statusCode = 200;
           res.setHeader('Content-Type', 'application/json');
           res.json({success: true, status: 'Registration Successful!'});
@@ -29,10 +30,12 @@ router.post('/signup', (req, res, next) => {
     })
 });
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
+router.post('/login', passport.authenticate('local', {session: false}), (req, res) => {
+
+  var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, status: 'You are successfully logged in!'});
+  res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
 router.get('/logout', (req, res) => {
