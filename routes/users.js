e6f78063976fad6a82e6094986cc6fb69bf3761f
Jai Agarwal <jai.agarwal2022@gmail.com>
2022-07-25 00:06:47 +0530

    CORS

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,15 @@ var express = require('express');
 const bodyParser = require('body-parser');
 const User = require('../models/user');
 var passport = require('passport');
-var authenticate = require('../authenticate')
+var authenticate = require('../authenticate');
+var cors = require('./cors')
 
 
 var router = express.Router();
 router.use(bodyParser.json());
 
 /* GET users listing. */
-router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+router.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
   User.find({})
   .then((users) => {
     res.statusCode = 200;
@@ -19,7 +20,7 @@ router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, (req, res, ne
   .catch((err) => next(err));
 });
 
-router.post('/signup', (req, res, next) => {
+router.post('/signup', cors.corsWithOptions,  (req, res, next) => {
   User.register(new User({ username: req.body.username }), req.body.password, (err, user) => {
     if (err) {
       res.statusCode = 500;
@@ -48,7 +49,7 @@ router.post('/signup', (req, res, next) => {
   });
 });
 
-router.post('/login', passport.authenticate('local', { session: false }), (req, res) => {
+router.post('/login', cors.corsWithOptions,  passport.authenticate('local', { session: false }), (req, res) => {
 
   var token = authenticate.getToken({ _id: req.user._id });
   res.statusCode = 200;
@@ -56,7 +57,7 @@ router.post('/login', passport.authenticate('local', { session: false }), (req, 
   res.json({ success: true, token: token, status: 'You are successfully logged in!' });
 });
 
-router.get('/logout', (req, res) => {
+router.get('/logout', cors.corsWithOptions, (req, res) => {
   if (req.session) {
     req.session.destroy();
     res.clearCookie('session-id');
