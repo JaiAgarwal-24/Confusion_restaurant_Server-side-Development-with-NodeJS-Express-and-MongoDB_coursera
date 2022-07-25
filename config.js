c983feb089499604f48c4eb66295597fa9a30e55
Jai Agarwal <jai.agarwal2022@gmail.com>
2022-07-25 18:11:28 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '2243365675815470',
+        clientSecret: 'cacafb4916be0d4b618cd8501c66dce6'
+    }
 }
